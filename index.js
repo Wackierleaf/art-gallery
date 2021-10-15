65252315303fb9d5f45cb73b32893e9a65e926b3
Roman Baklanov <wackier.leaf@yandex.ru>
2021-10-15 15:06:52 +0300

    Base project configuration finished

---
+++ index.js
@@ -0,0 +1,27 @@
+import express  from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import router from './router/index.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
+const start = async () => {
+    try {
+        await mongoose.connect(DB_URL);
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start().then();
