a27369e70ee7b8ccac2aea5a02f47d59aecf0287
Roman Baklanov <wackier.leaf@yandex.ru>
2021-10-18 15:55:19 +0300

    User login and logout functions finished

--- user/user.service.js
+++ user/user.service.js
@@ -32,6 +32,45 @@ class UserService {
         user.isActivated = true;
         await user.save();
     }
+
+    async login(email, password) {
+        const user = await userModel.findOne({email});
+        if (!user) {
+            throw ApiError.BadRequest(`User with ${email} not found`);
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest('Password is incorrect');
+        }
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {...tokens, user: userDto};
+    }
+
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
+
+    async refresh(refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const user = await userModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {...tokens, user: userDto};
+    }
 }
 
 export default new UserService();
