ba6320a3fb5ba282eb21b200a89d6145947f9093
Roman Baklanov <wackier.leaf@yandex.ru>
2021-11-25 16:11:13 +0300

    Finished top bar layout

--- frontend/src/app/layout-authorized/layout-authorized.module.ts
+++ frontend/src/app/layout-authorized/layout-authorized.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import {LayoutAuthorizedRoutingModule} from "./layout-authorized-routing.module";
 import { HeaderComponent } from './components/header/header.component';
+import {MaterialExModule} from "../tools/material.module";
+import {TranslateModule} from "@ngx-translate/core";
+import { MenuComponent } from './components/menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
     LayoutAuthorizedRoutingModule,
+    MaterialExModule,
+    TranslateModule
   ]
 })
 export class LayoutAuthorizedModule { }
