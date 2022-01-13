3c970cdf1c2f6a0be5082bfdec77457c8e728d83
Roman Baklanov <wackier.leaf@yandex.ru>
2022-01-13 14:10:17 +0300

    File uploader done

--- frontend/src/app/tools/material.module.ts
+++ frontend/src/app/tools/material.module.ts
@@ -31,9 +31,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
 
-
 @NgModule({
-  declarations: [],
   imports: [
     MatButtonModule,
     MatCardModule,
